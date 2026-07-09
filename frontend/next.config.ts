95e115bd5be692f53e569970a8ed8f8035b7dc30
LuisDaMich <luisda.michel@gmail.com>
2026-07-09 17:52:53 -0600

    hotfix dashboard assets

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  basePath: '/apps/projects/dashboard',
+  assetPrefix: '/lm-assets',
 };
 
 export default nextConfig;
